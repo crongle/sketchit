e8ef3dab3656a40dfd829d6618d13d28d86ba335
crongle <gasmazk73@gmail.com>
2019-03-10 16:17:05 +0000

    WIP - Grid reset

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+// Identifies the container for the grid to be made in
 const container = document.querySelector('.grid');
-
+// Initialised the internal grid container
 const sketchGrid = document.createElement('div');
-sketchGrid.classList.add('sketch-grid');
+sketchGrid.classList.add('sketch-grid', 'sketch-grid-layout');
 
-for  (i = 0; i < 256; ++i) {
-const sketchGridCell = document.createElement('div');
-sketchGridCell.classList.add('sketch-grid-cell');
-sketchGridCell.id = "cell" + i;
-sketchGridCell.textContent = 'sgc';
-sketchGridCell.addEventListener('mouseover', changeCellColor, false);
-sketchGrid.appendChild(sketchGridCell);
+// Draws the grid of cells based on input given
+function drawGrid(z) {
+  for  (a = 0; a < z; ++a) {
+    const sketchGridCell = document.createElement('div');
+    sketchGridCell.classList.add('sketch-grid-cell');
+    sketchGridCell.id = "cell" + a;
+    sketchGridCell.textContent = 'sgc';
+    sketchGridCell.addEventListener('mouseover', changeCellColor, false);
+    sketchGrid.appendChild(sketchGridCell);
+  }
 }
 
-container.appendChild(sketchGrid);
+// Resets drawn grids, prompts for size of new grid, draws new grid
+function gridReset() {
+  let gridToRemove = document.querySelector('.sketchGrid');
+  gridToRemove.parentNode.removeChild(gridToRemove);
 
+  let gridSize = prompt('How many units square would you like the next grid to be?');
+  sketchGrid.style.gridTemplateColumns=('repeat(' + gridSize + ', 40px)');
+  sketchGrid.style.gridTemplateRows=('repeat(' + gridSize + ', 40px)');
+  gridSize = gridSize * gridSize;
+  drawGrid(gridSize);
+  container.appendChild(sketchGrid);
+}
+
+// Colors cell backgrounds to black
 function changeCellColor(e) {
-  e.target.classList.add('colored-cell')
+  e.target.classList.add('colored-cell');
 }
+
+drawGrid(64);
